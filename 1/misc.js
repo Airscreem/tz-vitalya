35c0bf1b99d54bbe51663fbe25eaad4051ea7f92
Airscreem <polar5k@mail.ru>
2016-05-23 15:42:30 +0300

    re first task

--- 1/misc.js
+++ 1/misc.js
@@ -1,28 +1,31 @@
 function getSequence(number) {
-	var array = [1],
-		i, j,
+
+	var array = [1];
+
+	return function(number) {
+		var i, j,
 		max = array.length - 1;
 
-	if(number > max) {
-		for(i = max; i < number; i++) {
-			var s = array.slice(-3),
-				count = 0;
-			//console.log('slice ' + s + ' / length ' + s.length);
+		if(number > max) {
+			for(i = max; i < number; i++) {
+				var s = array.slice(-3),
+					count = 0;
+				//console.log('slice ' + s + ' / length ' + s.length);
 
-			for(j = 0; j < s.length; j++) {
-				count += parseInt(s[j]);
-				//console.log('count ' + count);
+				for(j = 0; j < s.length; j++) {
+					count += parseInt(s[j]);
+					//console.log('count ' + count);
+				}
+
+				array.push(count);
+				//console.log('push ' + array);
 			}
-
-			array.push(count);
-			//console.log('push ' + array);
 		}
-	}
 
-	return console.log(array[number], array);
+		return console.log(array[number-1]);
+	}
 }
 
 
-getSequence(5);
-getSequence(3);
-getSequence(7);
+var seq = getSequence();
+seq(3);
