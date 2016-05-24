7c3685f20c13f0606762359d772756ae17e3f13f
Airscreem <polar5k@mail.ru>
2016-05-24 16:30:23 +0300

    third task 3

--- 3/misc.js
+++ 3/misc.js
@@ -1,81 +1,76 @@
 (function(){
 
-	function CarouselController(array) {
-		var data = array;
+  function CarouselController(array) {
+    var data = array;
 
-		var pub = {
-			interval: 500,
-			auto: false,
-			init: init,
-			addSlider: addSlider,
-			removeSlider: removeSlider
-		};
+    var pub = {
+      interval: 500,
+      auto: false,
+      init: init,
+      nextClick: nextClick,
+      prevClick: prevClick,
+      addSlider: addSlider,
+      removeSlider: removeSlider
+    };
 
-		return pub;
+    return pub;
 
-		////////////////////////////////////
+    ////////////////////////////////////
 
-		function init() {
-      start();
-		}
-
-		function start() {
+    function init() {
       var length = data.length,
-          count = 0;
+      count = 0;
 
-      while(++count) {
-        if(auto) {
-          withInterval(count);
-        } else {
+      if(auto) {
+        var newCount = setInterval(function() {
+          count++;
+          next(count);
 
-        }
-        
-        if(count >= length) count = 0;
+          if(count >= length) count = 0;
+
+        }, this.interval);
       }
-		}
-
-    function withInterval(i) {
-      setTimeout(function() {
-        hide(data[i-1]);
-        show(data[i]);
-      }, pub.interval);
     }
 
-    function withoutInterval(i) {
-      
-    }
-
-    function showNext(i) {
-      i--;
-      hide(data[i+1]);
-      show(data[i]);
-    }
-
-    function showPrev(i) {
-      i++;
+    function next(i) {
       hide(data[i-1]);
       show(data[i]);
     }
 
-		function show(item) {
+    function prev(i) {
+      hide(data[i+1]);
+      show(data[i]);
+    }
+
+    function nextSlide() {
+
+    }
+
+    function prevSlide() {
+      
+    }
+
+
+
+    function show(item) {
       item.style.opacity = '1';
-		}
+    }
 
-		function hide(item) {
+    function hide(item) {
       item.style.opacity = '0';
-		}
+    }
 
-		function addSlider(item) {
-			data.push(item);
-		}
+    function addSlider(item) {
+      data.push(item);
+    }
 
-		function removeSlider(start, end) {
+    function removeSlider(start, end) {
       if(end === 'undefined') end = 0;
 
-			if(start > data.length || start > end) return console.log('Вы не можете удалить, попробуйте ещё разок!!!');
+      if(start > data.length || start > end) return console.log('Вы не можете удалить, попробуйте ещё разок!!!');
 
-			data.splice(start, end);
-		}
-	}
+      data.splice(start, end);
+    }
+  }
 
 })();
