7ccc455697c441fb0332daa000bb13577ab3f172
Airscreem <polar5k@mail.ru>
2016-05-30 13:36:43 +0300

    4 task 3

--- 4/misc.js
+++ 4/misc.js
@@ -15,12 +15,9 @@
 
   function Shape() {
     var pub = {
-      type: type,
-      lineWidth: lineWidth,
-      color: color,
       ctx: ctx,
       draw: draw
-    };s
+    };
 
     return pub;
 
@@ -30,11 +27,43 @@
       return newContext;
     }
 
-    function draw(point, ctx) {
-      ctx.fillStyle = this.color;
-      ctx.lineWidth = this.lineWidth;
-      ctx.type(point);
+    function draw(points, ctx) {
+      var self = this;
+      
+      ctx.fillStyle = this.color || 'blue';
+      ctx.lineWidth = this.lineWidth || 1;
+
+
+      if(points.length > 0) {
+        points.forEach(function(item) {
+
+          switch(self.type) {
+            case 'fillRect':
+              ctx.fillRect(item.x, item.y, item.width, item.height);
+              break;
+
+            case 'strokeRect':
+              ctx.strokeRect(item.x, item.y, item.width, item.height);
+              break;
+
+            case 'clearRect':
+              ctx.clearRect(item.x, item.y, item.width, item.height);
+              break;
+
+            default:
+              ctx.fillRect(item.x, item.y, item.width, item.height);
+              break;
+          }
+        });
+      }
     }
   }
+
+  var s = new Shape();
+  var mass = [new Point(2,3,5,5), new Point(10,10,5,5)];
+
+  s.color = 'red';
+
+  s.draw(mass, s.ctx('test', '2d'));
 })();
 
