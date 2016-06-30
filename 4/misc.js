8764f38a1d744cfc17f041b79f13211f33f4c554
Airscreem <polar5k@mail.ru>
2016-06-30 13:10:03 +0300

    4 task 7

--- 4/misc.js
+++ 4/misc.js
@@ -10,8 +10,16 @@
   }
 
   function ctx(id, context) {
-    var canvas = document.getElementById(id);
-    var newContext = canvas ? canvas.getContext(context) : document.createElement('canvas').id(id).getContext(context);
+    var canvas = document.getElementById(id),
+        newContext;
+
+    if(canvas) {
+      newContext = canvas.getContext(context);
+    } else {
+      newContext = document.createElement('canvas').id(id).getContext(context);
+      document.body.insertBefore(newContext, document.body.lastChild);
+    }
+
     return newContext;
   }
 
@@ -21,14 +29,17 @@
     self.arrayPoints = points;
     self.draw = draw;
 
-    function draw(ctx) {
-      //здесь мы рисуем любую фигуру по точками которые будут у нас в массиве полученны.
+    function draw() {
+      console.log('Our square', this);
     }
   }
 
-  Shape.Square = function(params) {
-    var ctx = params.ctx,
-        type = params.type;
+  /*Shape.prototype.draw = function(ctx) {
+    console.log('Our square', this);
+    .ctx = ctx;
+    
+
+    type = params.type;
 
     // Задаем свойства заливки и линий.
     ctx.fillStyle = params.color;
@@ -54,18 +65,32 @@
       default:
         ctx.fill(); ctx.stroke(); break;
     }
+  };*/
+
+  Shape.square = function(params) {
+    var obj = {
+      points: params.points,
+      type: params.type,
+      color: params.color,
+      size: params.size,
+      lineWidth: params.lineWidth
+    }
+
+    return obj;
   }
 
-  Shape.Square({
+  var s = [new Point(100, 200), new Point(100, 200), new Point(100, 200)];
+
+  var square = Shape.square({
+    points: s,
     lineWidth: 1,
     size: 100,
     color: "blue",
-    type: 'fill',
-    ctx: ctx('test', '2d')
+    type: 'fill'
   });
+  
+  console.log(square, Shape);
 
-  //а вот тут типа вызов просто рисования пообнрой фигуры по точкам.
-  //var s = new Shape([new Point(100, 200)], [new Point(100, 200)], [new Point(100, 200)]);
-  //s.draw(ctx('test', '2d'));
+  square.draw(ctx('test', '2d'));
 })();
 
